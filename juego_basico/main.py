b07c8c84c23e7a4c535992c852f28d8fc42e5652
kami-crs <caceresortizkamilavanessa@gmail.com>
2025-01-23 21:27:03 -0300

    Agregamos texto e imagenes en el juego

--- juego_basico/main.py
+++ juego_basico/main.py
@@ -5,11 +5,28 @@ pygame.init()
 # display.set_mode sirve para crear la pantalle con un ancho y alto
 pantalla = pygame.display.set_mode((640, 480))
 
+# font.Font sirve para crear la fuente
+fuente = pygame.font.Font(None, size=30)
+
+# cargar imagen
+mario_imagen = pygame.image.load("mario.png").convert_alpha()
+mario_imagen = pygame.transform.scale(mario_imagen, (64, 64))
+# cargar imagen de dogo
+dogo_imagen = pygame.image.load("dogo.jpg")
+dogo_imagen = pygame.transform.scale(dogo_imagen, (100, 100))
+
 # running se usa como bandera para mantener activo el while
 running = True
 while running:
     # pantalla.fill sirve para elegir el color de la pantalla en RGB
     pantalla.fill((205, 205, 205))
+    # mostrar texto
+    texto = fuente.render("Hello world", True, pygame.Color("#ff4500"))
+    pantalla.blit(texto, (250, 200))
+    # mostrar imagen de mario
+    pantalla.blit(mario_imagen, (0, 0))
+    # mostrar imagen de dogo
+    pantalla.blit(dogo_imagen, (100, 100))
     # pymage.event.get() devuelve una lista de eventos pueden ser teclados o mouse
     for event in pygame.event.get():
         # determina que el evento es de tipo teclado presionado
